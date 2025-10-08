3ba585ae07ba42ccd771823a8c55ba646ff416e0
Aruan Galves Amaral <aruangalves@gmail.com>
2025-10-08 16:17:21 -0300

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 
+//this page represents the main route of the application
+//every route is represented by its folder name and accessed from the page.tsx file within
+//examples:
+// app/page.tsx --> / root route
+// app/about/page.tsx --> /about route
+
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <h1>Hello from page.tsx from the main &quot;app&quot; folder!</h1>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
