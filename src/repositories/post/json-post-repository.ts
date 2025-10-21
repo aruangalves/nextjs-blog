93774f366838d4bc1d90b0340cd7bdfa5ccc83e1
Aruan Galves Amaral <aruangalves@gmail.com>
2025-10-21 19:54:01 -0300

    findAllPosts json implementation

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -1,16 +1,44 @@
 import { PostModel } from '@/models/post/post-model';
 import { PostRepository } from './post-repository';
+import { resolve } from 'path';
+import { readFile } from 'fs/promises';
 
 const PROJECT_ROOT_DIR = process.cwd();
+const JSON_POSTS_FILEPATH = resolve(
+  PROJECT_ROOT_DIR,
+  'src',
+  'db',
+  'seed',
+  'posts.json',
+);
 
 export class JsonPostRepository implements PostRepository {
-  private async readFromDisk() {}
+  private async readFromDisk(): Promise<PostModel[]> {
+    const fileContent = await readFile(JSON_POSTS_FILEPATH, 'utf-8');
+    const jsonContent = JSON.parse(fileContent);
+    const { posts } = jsonContent;
+    return posts;
+  }
 
-  async findAll(): Promise<PostModel[]> {}
+  async findAll(): Promise<PostModel[]> {
+    const posts = await this.readFromDisk();
+    return posts;
+  }
 }
-
 export const postRepository = new JsonPostRepository();
 
-console.log('This should be the project dir:');
-console.log(PROJECT_ROOT_DIR);
-console.log('Hi there, would you like to sign my petition?');
+//Printing the content from the JSON file:
+//Method #1
+//postRepository.findAll().then((posts) => console.log(posts));
+//Method #2
+// (async () => {
+//   const posts = await postRepository.findAll();
+//   console.log(posts);
+// })();
+
+(async () => {
+  const posts = await postRepository.findAll();
+  posts.forEach((post) => {
+    console.log(post.id);
+  });
+})();
