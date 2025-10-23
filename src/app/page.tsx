9197b47d7d2e0ddce40e990a57d55b9914f310cd
Aruan Galves Amaral <aruangalves@gmail.com>
2025-10-23 20:54:08 -0300

    Container component for page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Container } from '@/components/Container';
 import { PostList } from '@/components/PostList';
 import { SpinLoader } from '@/components/SpinLoader';
 import { Suspense } from 'react';
@@ -12,9 +13,25 @@ export default function Home() {
   console.log('Message from the Home server component');
 
   return (
-    <div className='text-slate-900 bg-slate-100 min-h-screen dark:text-slate-100 dark:bg-slate-900'>
+    <Container>
       <header>
         <h1 className='text-6x1 font-bold text-center py-8'>Your page title</h1>
+
+        <p className='text-justify'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+          maxime. At, veritatis omnis debitis voluptatem soluta asperiores iure
+          illum cumque, magni nulla itaque culpa aliquam voluptate natus beatae
+          nihil in? Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+          magni iste explicabo, optio eum ducimus magnam quaerat sint velit eius
+          neque ad officia est voluptatem cumque odit natus alias repudiandae!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia iure
+          doloribus sapiente veritatis consectetur qui neque eius est, animi
+          mollitia quis, blanditiis tempora esse? Iusto at necessitatibus
+          expedita optio beatae! Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Ullam quod cumque dicta recusandae praesentium
+          provident dignissimos quo doloribus sint. Esse ad aperiam molestias
+          illum quam quis debitis eveniet magni placeat?
+        </p>
       </header>
 
       <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
@@ -26,6 +43,6 @@ export default function Home() {
       <footer className='row-start-3 flex gap-[24px] flex-wrap items-center justify-center'>
         <p>Here is your footer</p>
       </footer>
-    </div>
+    </Container>
   );
 }
