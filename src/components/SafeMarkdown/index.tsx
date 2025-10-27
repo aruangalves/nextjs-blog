35d98fefd2eb910239973ea1143b988cb8ce4211
Aruan Galves Amaral <aruangalves@gmail.com>
2025-10-27 17:49:55 -0300

    Added react-markdown for rendering post content, added rehypeSanitize and remarkGfm packages

---
+++ src/components/SafeMarkdown/index.tsx
@@ -0,0 +1,20 @@
+import ReactMarkdown from 'react-markdown';
+import rehypeSanitize from 'rehype-sanitize';
+import remarkGfm from 'remark-gfm';
+
+type SafeMarkdownProps = {
+  markdown: string;
+};
+
+export function SafeMarkdown({ markdown }: SafeMarkdownProps) {
+  return (
+    <section className='post-body py-12'>
+      <ReactMarkdown
+        rehypePlugins={[rehypeSanitize]}
+        remarkPlugins={[remarkGfm]}
+      >
+        {markdown}
+      </ReactMarkdown>
+    </section>
+  );
+}
