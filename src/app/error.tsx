78833ad274846745fa7d148a3f1b8c4adb124747
Aruan Galves Amaral <aruangalves@gmail.com>
2025-10-28 11:47:40 -0300

    Error boundary for the root route, deployed format

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -9,24 +9,20 @@ type RootErrorPageProps = {
   reset: () => void;
 };
 
-export default function RootErrorPage({ error, reset }: RootErrorPageProps) {
+export default function RootErrorPage({ error }: RootErrorPageProps) {
   useEffect(() => {
     console.log(error);
   }, [error]);
 
   return (
     <ErrorMessage
-      pageTitle={'Error | The Blog'}
-      contentTitle={'An error has occurred'}
+      pageTitle={'Internal Server Error | The Blog'}
+      contentTitle={'501 | Internal Server Error'}
       message={
-        <>
-          <p>
-            The server encountered an error while processing your request.{' '}
-            <Link href='/'>Return to home</Link> or click on the button below to
-            try again.
-          </p>
-          <button onClick={() => reset()}>Try again</button>
-        </>
+        <p>
+          The server encountered an error while processing your request.{' '}
+          <Link href='/'>Return home</Link> or try again later.
+        </p>
       }
     />
   );
