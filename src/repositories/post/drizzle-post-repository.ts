1e9edba7ad3751e0dc0010255de0aaca0f5c80a5
Aruan Galves Amaral <aruangalves@gmail.com>
2025-10-30 11:07:04 -0300

    Finished drizzleRepository methods, replaced jsonRepository with drizzleRepository

--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -1,6 +1,7 @@
 import { PostModel } from '@/models/post/post-model';
 import { PostRepository } from './post-repository';
 import { drizzleDb } from '@/db/drizzle';
+import { eq } from 'drizzle-orm';
 
 export class DrizzlePostRepository implements PostRepository {
   async findAllPublishedPublic(): Promise<PostModel[]> {
@@ -21,8 +22,32 @@ export class DrizzlePostRepository implements PostRepository {
 
     return posts;
   }
-  async findBySlugPublic(slug: string): Promise<PostModel> {}
 
-  async findAll(): Promise<PostModel[]> {}
-  async findById(id: string): Promise<PostModel> {}
+  async findBySlugPublic(slug: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq, and }) =>
+        and(eq(posts.published, true), eq(posts.slug, slug)),
+    });
+
+    if (!post) throw new Error('Post not found by the specified slug');
+
+    return post;
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    const posts = await drizzleDb.query.posts.findMany({
+      orderBy: (posts, { desc }) => desc(posts.createdAt),
+    });
+
+    return posts;
+  }
+  async findById(id: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq }) => eq(posts.id, id),
+    });
+
+    if (!post) throw new Error('Post not found by the specified id');
+
+    return post;
+  }
 }
