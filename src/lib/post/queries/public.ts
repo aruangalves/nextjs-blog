94942c3f0457320557e24206c001084a9a287271
Aruan Galves Amaral <aruangalves@gmail.com>
2025-10-31 14:09:25 -0300

    Added unstable_cache for public queries

--- src/lib/post/queries/public.ts
+++ src/lib/post/queries/public.ts
@@ -1,23 +1,34 @@
 import { postRepository } from '@/repositories/post';
 import { notFound } from 'next/navigation';
+import { unstable_cache } from 'next/cache';
 import { cache } from 'react';
 
 //React cache removes duplicated calls from the same API or repo
 //Useful when you need to invoke the same database query more than once
 export const findAllPublicPostsCached = cache(
-  async () => await postRepository.findAllPublishedPublic(),
+  unstable_cache(
+    async () => {
+      return await postRepository.findAllPublishedPublic();
+    },
+    ['posts'],
+    {
+      tags: ['posts'],
+    },
+  ),
 );
 
-export const findPublicPostBySlugCached = cache(async (slug: string) => {
-  const post = await postRepository
-    .findBySlugPublic(slug)
-    .catch(() => undefined);
+export const findPublicPostBySlugCached = cache((slug: string) => {
+  return unstable_cache(
+    async (slug: string) => {
+      const post = await postRepository
+        .findBySlugPublic(slug)
+        .catch(() => undefined);
 
-  if (!post) notFound();
+      if (!post) notFound();
 
-  return post;
+      return post;
+    },
+    [`post-${slug}`],
+    { tags: [`post-${slug}`] },
+  )(slug);
 });
-
-export const findPostByIdCached = cache(
-  async (id: string) => await postRepository.findById(id),
-);
