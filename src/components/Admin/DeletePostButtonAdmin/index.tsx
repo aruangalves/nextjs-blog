3426675f12c4c3dc88aa4437f757af83dc4ed788
Aruan Galves Amaral <aruangalves@gmail.com>
2025-11-01 20:37:38 -0300

    Defined useTransition hook to properly handle user feedback after click DeletePostButtonAdmin

--- src/components/Admin/DeletePostButtonAdmin/index.tsx
+++ src/components/Admin/DeletePostButtonAdmin/index.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { deletePostAction } from '@/actions/post/delete-post-action';
 import { Trash2Icon } from 'lucide-react';
+import { useTransition } from 'react';
 
 type DeletePostButtonAdminProps = {
   id: string;
@@ -11,11 +13,23 @@ export function DeletePostButtonAdmin({
   id,
   title,
 }: DeletePostButtonAdminProps) {
+  const [isPending, startTransition] = useTransition();
+
+  function handleClick() {
+    if (!confirm('Do you really want to delete this post?')) return;
+    startTransition(async () => {
+      const result = await deletePostAction(id);
+      alert(`Invoked server action to delete post with id ${result}`);
+    });
+  }
+
   return (
     <button
-      className='text-red-700 cursor-pointer transition hover:scale-120 hover:text-red-600'
+      className='text-red-700 cursor-pointer transition hover:scale-120 hover:text-red-600 disabled:text-gray-800 disabled:cursor-not-allowed'
       aria-label={`Delete post: ${title}`}
       title={`Delete post: ${title}`}
+      onClick={handleClick}
+      disabled={isPending}
     >
       <Trash2Icon />
     </button>
