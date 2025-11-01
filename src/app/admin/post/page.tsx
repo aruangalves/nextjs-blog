83ffe680764e90ff8653cb5d8049f4b760b2d97b
Aruan Galves Amaral <aruangalves@gmail.com>
2025-11-01 20:09:01 -0300

    Styled admin post listing

--- src/app/admin/post/page.tsx
+++ src/app/admin/post/page.tsx
@@ -1,19 +1,17 @@
 import { PostListAdmin } from '@/components/Admin/PostListAdmin';
 import { SpinLoader } from '@/components/SpinLoader';
-import { findAllPostsAdmin } from '@/lib/post/queries/admin';
 import { Metadata } from 'next';
 import { Suspense } from 'react';
 
 export const dynamic = 'force-dynamic';
 
 export const metadata: Metadata = {
-  title: 'Manage Posts | The Blog',
+  title: 'Manage Posts',
 };
 
 export default async function AdminPostPage() {
-  const posts = await findAllPostsAdmin();
   return (
-    <div className='py-16'>
+    <div>
       <h1 className='text-4xl'>To do... AdminPostPage</h1>
       <p>It requires some work...</p>
       <h2 className='text-2xl'>Testing database function...</h2>
--- src/components/Admin/PostListAdmin/index.tsx
+++ src/components/Admin/PostListAdmin/index.tsx
@@ -1,44 +1,60 @@
-import { PostCoverImage } from '../../PostCoverImage';
-import { PostHeading } from '../../PostHeading';
 import { PostDate } from '../../PostDate';
 import { findAllPostsAdmin } from '@/lib/post/queries/admin';
+import { Trash2Icon } from 'lucide-react';
+import Link from 'next/link';
 
 export async function PostListAdmin() {
   const posts = await findAllPostsAdmin();
 
   if (posts.length > 0) {
     return (
-      <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3'>
+      <div className='grid grid-cols-1 gap-1'>
+        <div className='grid grid-cols-[3fr_1fr_1fr_4rem] font-bold text-[1.05rem] gap-1 [&_p]:bg-slate-600 [&_p]:px-2 [&_p]:py-2 [&_p]:rounded-[0.15rem] [&_p]:text-slate-50'>
+          <p>Post title</p>
+          <p>Creation date</p>
+          <p>Last edit</p>
+          <p className='text-center'>Delete</p>
+        </div>
         {posts.map((post) => {
-          const postUrl = `/post/${post.slug}`;
+          const cssStyling = 'bg-slate-300 py-4 px-2 rounded-[0.15rem]';
           return (
-            <article key={post.id} className='flex flex-col gap-4 group'>
-              <PostCoverImage
-                linkProps={{
-                  href: postUrl,
-                }}
-                imageProps={{
-                  width: 1200,
-                  height: 720,
-                  src: post.coverImageUrl,
-                  alt: post.title,
-                }}
-              />
-              <PostDate
-                date={post.createdAt}
-                className='text-slate-600 text-sm/tight mb-0.5'
-              />
-              <PostHeading href={postUrl} as='h3'>
-                {post.title}
-              </PostHeading>
+            <article
+              key={post.id}
+              className='grid grid-cols-[3fr_1fr_1fr_4rem] gap-1'
+            >
+              <p className={cssStyling}>
+                <Link
+                  href={`/admin/post/${post.id}`}
+                  className='hover:underline'
+                >
+                  {post.title}
+                </Link>{' '}
+                {!post.published && (
+                  <span className='text-slate-600 italic text-sm'>
+                    (Não publicado)
+                  </span>
+                )}
+              </p>
 
-              <p>{post.excerpt}</p>
+              <PostDate date={post.createdAt} className={cssStyling} />
+
+              <PostDate date={post.updatedAt} className={cssStyling} />
+
+              <div className={`${cssStyling} flex flex-row justify-center`}>
+                <button
+                  className='text-red-700 cursor-pointer transition hover:scale-120 hover:text-red-600'
+                  aria-label={`Delete post: ${post.title}`}
+                  title={`Delete post: ${post.title}`}
+                >
+                  <Trash2Icon />
+                </button>
+              </div>
             </article>
           );
         })}
       </div>
     );
   } else {
-    <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3'></div>;
+    <div className='grid grid-cols-1 gap-8'></div>;
   }
 }
