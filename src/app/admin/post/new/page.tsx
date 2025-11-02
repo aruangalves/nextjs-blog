a9ea46aee92dea11fb76060bee26e14d270ea21b
Aruan Galves Amaral <aruangalves@gmail.com>
2025-11-02 17:27:11 -0300

    Button component for post create and edit forms

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,10 +1,13 @@
+import { Button } from '@/components/Button';
+
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
   return (
     <div className='py-16'>
-      <h1 className='text-4xl'>To do... AdminPostNewPage</h1>
-      <p>It requires some work...</p>
+      <h1 className='text-4xl mb-4'>To do... AdminPostNewPage</h1>
+      <p className='mb-4'>It requires some work...</p>
+      <Button>Some action</Button>
     </div>
   );
 }
