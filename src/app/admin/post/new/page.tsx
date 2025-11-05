3946af7580d72c10e621391cc0eac88361049e12
Aruan Galves Amaral <aruangalves@gmail.com>
2025-11-04 22:35:16 -0300

    Defined 3 predefined styles for Button component

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -7,7 +7,21 @@ export default async function AdminPostNewPage() {
     <div className='py-16'>
       <h1 className='text-4xl mb-4'>To do... AdminPostNewPage</h1>
       <p className='mb-4'>It requires some work...</p>
-      <Button>Some action</Button>
+      <div className='py-16 flex gap-4 flex-wrap'>
+        <Button variant='default'>Some action</Button>
+        <Button variant='ghost'>Some action</Button>
+        <Button variant='danger'>Some action</Button>
+
+        <Button variant='default' disabled>
+          Some action
+        </Button>
+        <Button variant='ghost' disabled>
+          Some action
+        </Button>
+        <Button variant='danger' disabled>
+          Some action
+        </Button>
+      </div>
     </div>
   );
 }
