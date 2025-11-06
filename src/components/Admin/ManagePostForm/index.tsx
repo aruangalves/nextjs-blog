07402e188c09d73b05d7b38ec763eb7b62e0e115
Aruan Galves Amaral <aruangalves@gmail.com>
2025-11-06 19:54:50 -0300

    Added markdown editor for creating and editing blog post content

--- src/components/Admin/ManagePostForm/index.tsx
+++ src/components/Admin/ManagePostForm/index.tsx
@@ -3,12 +3,23 @@
 import { Button } from '@/components/Button';
 import { InputCheckbox } from '@/components/InputCheckbox';
 import { InputText } from '@/components/InputText';
+import { MarkdownEditor } from '@/components/MarkdownEditor';
+import { useState } from 'react';
 
 export function ManagePostForm() {
+  const [contentValue, setContentValue] = useState('Este é **um exemplo**.');
+
   return (
     <form action='' className='mb-16'>
       <InputText />
       <InputCheckbox />
+      <MarkdownEditor
+        labelText='Conteúdo'
+        disabled={false}
+        textAreaName='content'
+        value={contentValue}
+        setValue={setContentValue}
+      />
       <Button />
     </form>
   );
