780fb114f7a119c2ce9da588a500f2c766696be7
Aruan Galves Amaral <aruangalves@gmail.com>
2025-11-09 18:03:01 -0300

    Server action now correctly uploads image file into server

--- src/actions/upload/upload-image-action.ts
+++ src/actions/upload/upload-image-action.ts
@@ -1,6 +1,12 @@
 'use server';
 
-import { IMAGE_UPLOAD_MAX_SIZE } from '@/lib/constants';
+import {
+  IMAGE_SERVER_URL,
+  IMAGE_UPLOAD_DIRECTORY,
+  IMAGE_UPLOAD_MAX_SIZE,
+} from '@/lib/constants';
+import { mkdir, writeFile } from 'fs/promises';
+import { extname, resolve } from 'path';
 
 type UploadImageActionResult = {
   url: string;
@@ -10,6 +16,7 @@ type UploadImageActionResult = {
 export async function uploadImageAction(
   formData: FormData,
 ): Promise<UploadImageActionResult> {
+  //TODO: check user login before proceeding
   const makeResult = ({ url = '', error = '' }) => {
     return { url, error };
   };
@@ -33,10 +40,30 @@ export async function uploadImageAction(
     });
   }
 
-  //Note: production validation requires a lib to really assert if the file has a valid type, checking only through mime-types is not enough!
+  //Note: production validation requires a lib to really assert if the file has a valid type, checking only through mime-types is not recommended!
   if (!file.type.startsWith('image/')) {
     return makeResult({ error: 'Formato de imagem inválido' });
   }
 
-  return makeResult({ url: 'URL' });
+  const imageExtension = extname(file.name);
+  const uniqueImageName = `${Date.now()}${imageExtension}`;
+
+  const uploadFullPath = resolve(
+    process.cwd(),
+    'public',
+    IMAGE_UPLOAD_DIRECTORY,
+  );
+
+  await mkdir(uploadFullPath, { recursive: true });
+
+  const fileArrayBuffer = await file.arrayBuffer();
+  const buffer = Buffer.from(fileArrayBuffer);
+
+  const fileFullPath = resolve(uploadFullPath, uniqueImageName);
+
+  await writeFile(fileFullPath, buffer);
+
+  const url = `${IMAGE_SERVER_URL}/${IMAGE_UPLOAD_DIRECTORY}/${uniqueImageName}`;
+
+  return makeResult({ url });
 }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,4 @@
 export const SIMULATE_WAIT_IN_MS = 0;
 export const IMAGE_UPLOAD_MAX_SIZE = 921600; //1024 * 900
+export const IMAGE_UPLOAD_DIRECTORY = 'uploads'; // ./public/uploads
+export const IMAGE_SERVER_URL = 'http://localhost:3000'; //localhost:3000/uploads/image-name.jpeg
