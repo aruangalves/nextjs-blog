2c8024f6baf225afc6d91710e490ef3288871732
Aruan Galves Amaral <aruangalves@gmail.com>
2025-11-09 16:44:50 -0300

    Initial image file validation for ImageUploader component

--- src/components/Admin/ImageUploader/index.tsx
+++ src/components/Admin/ImageUploader/index.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { Button } from '@/components/Button';
+import { IMAGE_UPLOAD_MAX_SIZE } from '@/lib/constants';
 import { ImageUpIcon } from 'lucide-react';
 import { useRef } from 'react';
+import { toast } from 'react-toastify';
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -12,6 +14,36 @@ export function ImageUploader() {
     if (!fileInputRef.current) return;
 
     fileInputRef.current.click();
+  }
+
+  function handleInputFileChange() {
+    if (!fileInputRef.current) return;
+
+    const fileInput = fileInputRef.current;
+
+    const file = fileInput?.files?.[0];
+
+    if (!file) return;
+
+    if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
+      toast.error(
+        `O tamanho da imagem excede o limite de upload (${
+          IMAGE_UPLOAD_MAX_SIZE / 1024
+        }KB), por favor escolha uma imagem menor.`,
+      );
+
+      fileInput.value = '';
+
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    //TODO: Criar a action para upload de arquivo
+    console.log(formData.get('file'));
+
+    fileInput.value = '';
   }
 
   return (
@@ -26,6 +58,7 @@ export function ImageUploader() {
         className='hidden'
         accept='image/*'
         ref={fileInputRef}
+        onChange={handleInputFileChange}
       />
     </div>
   );
