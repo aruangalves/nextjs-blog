896f617808d8cc44cd960b1103d4af3e9e45385a
Aruan Galves Amaral <aruangalves@gmail.com>
2025-11-10 20:03:03 -0300

    Inserted fields into ManagePostForm component

--- src/components/Admin/ManagePostForm/index.tsx
+++ src/components/Admin/ManagePostForm/index.tsx
@@ -12,16 +12,63 @@ export function ManagePostForm() {
   const [contentValue, setContentValue] = useState('Este é **um exemplo**.');
 
   return (
-    <form action='' className='mb-16 flex flex-col gap-8'>
-      <InputText />
-      <InputCheckbox />
-      <ImageUploader />
+    <form action='' className='mb-16 flex flex-col gap-4'>
+      <InputText
+        labelText='ID'
+        name='id'
+        placeholder='ID gerado automaticamente'
+        type='text'
+        readOnly
+        defaultValue={''}
+      />
+      <InputText
+        labelText='Slug'
+        name='slug'
+        placeholder='Slug gerado automaticamente'
+        type='text'
+        readOnly
+        defaultValue={''}
+      />
+      <InputText
+        labelText='Autor'
+        name='author'
+        placeholder='Digite o nome do autor'
+        type='text'
+        defaultValue={''}
+      />
+      <InputText
+        labelText='Título'
+        name='title'
+        placeholder='Digite o título'
+        type='text'
+        defaultValue={''}
+      />
+      <InputText
+        labelText='Excerto'
+        name='excerpt'
+        placeholder='Digite o resumo'
+        type='text'
+        defaultValue={''}
+      />
       <MarkdownEditor
         labelText='Conteúdo'
         disabled={false}
         textAreaName='content'
         value={contentValue}
         setValue={setContentValue}
+      />
+      <ImageUploader />
+      <InputText
+        labelText='URL da imagem de capa'
+        name='coverImageUrl'
+        placeholder='Digite a URL da imagem de capa'
+        type='text'
+        defaultValue={''}
+      />
+      <InputCheckbox
+        labelText='Publicar post?'
+        name='published'
+        type='checkbox'
       />
       <Button type='submit'>
         <MessageSquarePlusIcon />
