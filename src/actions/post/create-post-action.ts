3892860e35ff0192e1b602964b4ec5c625503079
Aruan Galves Nascimento Amaral <aruanamaral@secties.local>
2025-11-11 13:13:05 -0300

    Added create, update and delete methods for post-repository interface, added them into create and delete server actions

--- src/actions/post/create-post-action.ts
+++ src/actions/post/create-post-action.ts
@@ -1,10 +1,9 @@
 'use server';
 
-import { drizzleDb } from '@/db/drizzle';
-import { postsTable } from '@/db/drizzle/schemas';
 import { makePartialPublicPost, PublicPost } from '@/dto/post/dto';
 import { PostCreateSchema } from '@/lib/post/validations';
 import { PostModel } from '@/models/post/post-model';
+import { postRepository } from '@/repositories/post';
 import { getZodErrorMessages } from '@/utils/get-zod-error-msgs';
 import { makeSlugFromText } from '@/utils/make-slug-from-text';
 import { revalidateTag } from 'next/cache';
@@ -48,8 +47,21 @@ export async function createPostAction(
     slug: makeSlugFromText(validPostData.title),
   };
 
-  //TODO: move this method to the repo
-  await drizzleDb.insert(postsTable).values(newPost);
+  try {
+    await postRepository.create(newPost);
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return {
+        formState: newPost,
+        errors: [e.message],
+      };
+    }
+
+    return {
+      formState: newPost,
+      errors: ['Erro desconhecido'],
+    };
+  }
 
   revalidateTag('posts');
   redirect(`/admin/post/${newPost.id}`);
--- src/actions/post/delete-post-action.ts
+++ src/actions/post/delete-post-action.ts
@@ -1,20 +1,10 @@
 'use server';
 
-import { drizzleDb } from '@/db/drizzle';
-import { postsTable } from '@/db/drizzle/schemas';
 import { postRepository } from '@/repositories/post';
-import { asyncDelay } from '@/utils/async-delay';
-import { logColor } from '@/utils/log-color';
-import { eq } from 'drizzle-orm';
 import { revalidateTag } from 'next/cache';
 
 export async function deletePostAction(id: string) {
   //TODO: check user login credentials before proceeding
-
-  //TODO2: remove these lines:
-  await asyncDelay(2000);
-  logColor('This will delete post: ' + id);
-  //TODO2-END
 
   if (!id || typeof id !== 'string') {
     return {
@@ -22,18 +12,16 @@ export async function deletePostAction(id: string) {
     };
   }
 
-  const post = await postRepository.findById(id).catch(() => undefined);
-
-  if (!post) {
-    return {
-      error: 'Post not found.',
-    };
+  let post;
+  try {
+    post = await postRepository.delete(id);
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return e;
+    }
+    return 'Erro desconhecido';
   }
 
-  //TODO3: invoke deletion query
-  await drizzleDb.delete(postsTable).where(eq(postsTable.id, id));
-
-  logColor('This post should be deleted now: ' + id);
   revalidateTag('posts');
   revalidateTag(`post-${post.slug}`);
 
